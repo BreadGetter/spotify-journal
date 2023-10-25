737b54f88c64f40190b3972f5f1a6220b5921940
Maxim <maximcool39@gmail.com>
2023-10-25 20:01:39 +0200

    backing up files

--- backend/config.py
+++ backend/config.py
@@ -6,8 +6,7 @@ load_dotenv(os.path.join(basedir, '.env')) # load environment variables from .en
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET-KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID') 
     SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
